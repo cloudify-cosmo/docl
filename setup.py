3d8b1634660d3cae2c09d9c1c074cc8ecc796825
Dan Kilman <dankilman@gmail.com>
2016-08-15 13:14:43 +0300

    update docker file

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='docl',
-    version='0.1',
+    version='0.2',
     author='GigaSpaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=[
