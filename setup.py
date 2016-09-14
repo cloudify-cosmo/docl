af98cdf33d141960934129286d982212acb3ed9a
Dan Kilman <dankilman@gmail.com>
2016-09-14 17:37:15 +0300

    bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='docl',
-    version='0.27',
+    version='0.28',
     author='GigaSpaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=[
