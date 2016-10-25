569a2f42d9c9c8d43c47548fc78f80a52a4d6bbb
Pavel Brodsky <mcouthon@users.noreply.github.com>
2016-10-25 14:10:09 +0300

    Bump docl version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='docl',
-    version='0.35',
+    version='0.36',
     author='GigaSpaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=[
